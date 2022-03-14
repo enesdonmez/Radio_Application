05ef23915791d4ec5defbd498cae310e204eec74
Enes DÖNMEZ <77546008+enesdonmez@users.noreply.github.com>
2022-03-14 15:20:46 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace RadioApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.WindowsMP = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WindowsMP
+            // 
+            this.WindowsMP.Enabled = true;
+            this.WindowsMP.Location = new System.Drawing.Point(13, 11);
+            this.WindowsMP.Name = "WindowsMP";
+            this.WindowsMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WindowsMP.OcxState")));
+            this.WindowsMP.Size = new System.Drawing.Size(276, 191);
+            this.WindowsMP.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Location = new System.Drawing.Point(612, 111);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 91);
+            this.button6.TabIndex = 4;
+            this.button6.Text = " ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::RadioApplication.Properties.Resources.ntv;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(460, 111);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 91);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::RadioApplication.Properties.Resources.istanbul;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(309, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 91);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::RadioApplication.Properties.Resources._2000;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(612, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 91);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::RadioApplication.Properties.Resources.acık;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(460, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 91);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::RadioApplication.Properties.Resources.radyo_seymen_dinle;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(309, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 91);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(802, 304);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.WindowsMP);
+            this.Name = "Form1";
+            this.Text = "RADYO UYGULAMAM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMP)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer WindowsMP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace RadioApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WindowsMP.URL = "https://vofile.ru/turkey/radyo_2000_fm_istanbul/icecast";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WindowsMP.URL = "https://stream2.rtvtakip.com:7000/stream?icy=http";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WindowsMP.URL = "https://wowza1.radyotvonline.com/unitedmedia/karade";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowsMP.URL = "https://stream.34bit.net/ar.opus";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            WindowsMP.URL = "http://2.59.117.43:2020/;stream.mp3";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            WindowsMP.URL = "http://46.20.7.126/;stream.mp3";
+        }
+
+        
+        }
+    }
+
